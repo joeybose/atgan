40ebcd97c36ee7312856f0b69146b94cd513730a
Joey Bose <joey.bosr@gmail.com>
2017-11-11 21:35:39 -0500

    First attempt at an attack based on GAT

---
+++ dcgan/dcgan_attack.py
@@ -0,0 +1,282 @@
+from __future__ import print_function
+import pdb
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torch.autograd as autograd
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw | fake')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=128, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=48)
+parser.add_argument('--ndf', type=int, default=48)
+parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0001, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--outf', default='./results/', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+
+opt = parser.parse_args()
+print(opt)
+
+alpha = 0.5
+c_g = 0.16
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+if opt.cuda:
+    torch.cuda.manual_seed_all(opt.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+if opt.dataset in ['imagenet', 'folder', 'lfw']:
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transforms.Compose([
+                                   transforms.Scale(opt.imageSize),
+                                   transforms.CenterCrop(opt.imageSize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+elif opt.dataset == 'lsun':
+    dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
+                        transform=transforms.Compose([
+                            transforms.Scale(opt.imageSize),
+                            transforms.CenterCrop(opt.imageSize),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        ]))
+elif opt.dataset == 'cifar10':
+    dataset = dset.CIFAR10(root=opt.dataroot, download=True,
+                           transform=transforms.Compose([
+                               transforms.Scale(opt.imageSize),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+elif opt.dataset == 'fake':
+    dataset = dset.FakeData(image_size=(3, opt.imageSize, opt.imageSize),
+                            transform=transforms.ToTensor())
+assert dataset
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+                                         shuffle=True, num_workers=int(opt.workers))
+
+ngpu = int(opt.ngpu)
+nz = int(opt.nz)
+ngf = int(opt.ngf)
+ndf = int(opt.ndf)
+nc = 3
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+class _netG(nn.Module):
+    def __init__(self, ngpu):
+        super(_netG, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(nc, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 48 x 32 x 32
+            nn.Conv2d(ngf, ngf, 1, 1, 0, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 3 x 32 x 32
+            nn.Conv2d(ngf, nc, 1, 1, 0, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+
+netG = _netG(ngpu)
+netG.apply(weights_init)
+if opt.netG != '':
+    netG.load_state_dict(torch.load(opt.netG))
+print(netG)
+
+
+class _netD(nn.Module):
+    def __init__(self, ngpu):
+        super(_netD, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(nc, ndf, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf, 3, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf*2, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*2, ndf*2, 3, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*2, ndf*2, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*2, ndf*2, 1, 1, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*2, 10, 1, 1, 1, bias=False),
+            nn.AvgPool2d((8,8)),
+            nn.Softmax()
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        return output.squeeze()
+
+def one_hot(batch_size,labels):
+    zeros = torch.zeros(batch_size,10)
+    return zeros.scatter_(dim,index,1)
+
+netD = _netD(ngpu)
+netD.apply(weights_init)
+if opt.netD != '':
+    netD.load_state_dict(torch.load(opt.netD))
+print(netD)
+
+criterion = nn.CrossEntropyLoss()
+
+input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
+noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
+fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
+label = torch.FloatTensor(opt.batchSize)
+real_label = 1
+fake_label = 0
+
+if opt.cuda:
+    netD.cuda()
+    netG.cuda()
+    criterion.cuda()
+    input, label = input.cuda(), label.cuda()
+    noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+
+fixed_noise = Variable(fixed_noise)
+
+# setup optimizer
+optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+for epoch in range(opt.niter):
+    for i, batch in enumerate(dataloader):
+        data, classes = batch[0], batch[1]
+
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # train with real
+        netD.zero_grad()
+        real_cpu = data
+        batch_size = real_cpu.size(0)
+        if opt.cuda:
+            real_cpu = real_cpu.cuda()
+        input.resize_as_(real_cpu).copy_(real_cpu)
+        label.resize_(batch_size).fill_(real_label)
+        inputv = Variable(input)
+        inputv.requires_grad = True
+        labelv = Variable(classes).cuda()
+        output = netD(inputv)
+        errD_real = criterion(output, labelv)
+        errD_real.backward(retain_graph=True)
+        D_x = output.data.mean()
+
+        # train with fake
+        #noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
+        #noisev = Variable(noise)
+
+        fake = netG(Variable(inputv.grad.data))
+        #inputv.grad.data.zero_()
+        perturbed = inputv + fake
+        #perturbed.requires_grad = True
+        output_fake = netD(perturbed)
+        #output_fake.volatile = False
+        errD_fake = criterion(output_fake, labelv)
+        errD_fake.backward(retain_graph=True)
+        D_G_z1 = output_fake.data.mean()
+        errD = alpha*errD_real + (1-alpha)*errD_fake
+        #errD.backward()
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        output = output_fake + c_g * torch.norm(fake,2).pow(2)
+        errG = criterion(output, labelv)
+        errG.backward()
+        D_G_z2 = output.data.mean()
+        optimizerG.step()
+
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+              % (epoch, opt.niter, i, len(dataloader),
+                 errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+        if i % 100 == 0:
+            vutils.save_image(real_cpu,
+                    '%s/real_samples.png' % opt.outf,
+                    normalize=True)
+            fake = netG(Variable(inputv.grad.data)) + inputv
+            vutils.save_image(fake.data,
+                    '%s/images/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
+                    normalize=True)
+
+    # do checkpointing
+    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
+    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
