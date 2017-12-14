16dd78b0cbc0629b28c108fbcd654be64dea3979
romina72 <romina.abachi@mail.utoronto.ca>
2017-12-13 20:14:10 -0500

    Update example.py

--- attacks/example.py
+++ attacks/example.py
@@ -39,10 +39,10 @@ def load_cifar():
 	    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 	])
 
-	trainset = torchvision.datasets.CIFAR10(root='/scratch/data', train=True, download=True, transform=transform_train)
+	trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 	trainloader = torch.utils.data.DataLoader(trainset,batch_size=1024,shuffle=True, num_workers=8)
 
-	testset = torchvision.datasets.CIFAR10(root='/scratch/data', train=False, download=True, transform=transform_test)
+	testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 	testloader = torch.utils.data.DataLoader(testset,batch_size=128,shuffle=False, num_workers=8)
 	return trainloader, testloader
 
